0d6fbd0850de14abc308daf7313f0a8874501968
SleepyBadMan <1187692525@qq.com>
2016-11-28 19:36:05 +0800

    5 init

--- code/5LongestPalindromicSubstring.py
+++ code/5LongestPalindromicSubstring.py
@@ -4,8 +4,32 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        s = self.toListWithSeparator(s)
 
-    def toListWithSeparator(self,s,ch):
+        p = [1]*len(s) #存储以s[i]为中心的回文半径
+        p[0]=1
+        p[1]=2
+
+
+        mx=2   # mx = id+p[i]
+        id=1
+
+        for i in range(3,len(s)):
+            if(mx+id>i):
+                p[i] =  min(p[2*id - i],mx-i)
+
+
+
+
+
+
+
+
+    def toListWithSeparator(self,s:str):
+        #将字符串s插入分隔符ch并转化为list
+        s = list(s)
+        s.sort()
+        ch = chr(ord(s[-1])+1)
         res = list()
         for c in s:
             res.append(ch)
@@ -17,4 +41,4 @@ class Solution(object):
 
 a = str("asdfggfdsa")
 one = Solution()
-print(one.toListWithSeparator(a,"#"))
+print(one.toListWithSeparator(a))
