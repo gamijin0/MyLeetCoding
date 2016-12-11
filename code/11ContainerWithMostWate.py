7560c3bdcd9aa4109f4d0205f950a1f83c6375a3
chaos <497386856@qq.com>
2016-12-11 16:25:48 +0800

    finish 11. Container With Most Water

---
+++ code/11ContainerWithMostWate.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+
+
+        i=0
+        j=len(height)-1
+        maxArea = 0
+
+        while(i<j):
+            maxArea = max(maxArea,(j-i)*min(height[i],height[j]))
+            if(height[i]<height[j]):
+                i+=1
+            else:
+                j-=1
+
+        return maxArea
